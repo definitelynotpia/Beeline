d9ef4b847f76857a5d9af4dd4c51cb514ec33081
CIIT502PC13\Student <it@ciit.edu.ph>
2024-02-26 12:44:34 +0800

    connected to firestore database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./components/Dashboard";
 // react
 import "./App.css";
 import logo from "./logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Routes, Route, NavLink } from "react-router-dom";
 // firebase
@@ -20,8 +20,19 @@ function App() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  console.log(process.env);
-  console.log(process.env.REACT_APP_BEELINE_API_KEY);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "Users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+
+    getUsers();
+  }, []);
 
   const accounts = [
     { username: "harrystyles", email: "harry.styles@school.edu.com", password: "asitwas" },
@@ -47,51 +58,65 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {/* Navbar */}
-      <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div className="container-fluid">
-
-          <img src={logo} alt="BEELINE logo" width="50" className="d-inline-block align-text-top app-icon" />
-          <NavLink className="navbar-brand text-warning app-name" to="/">BEELINE</NavLink>
-
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link active" to="/">Dashboard</NavLink>
-              </li>
-            </ul>
-
-            {isLogin && <><button className="btn btn-outline-warning ms-5 me-3" id="loginButton" type="button">
-              <NavLink className="nav-link" to="/login">Login</NavLink>
-            </button>
-              <button className="btn btn-warning" id="registerButton" type="button">
-                <NavLink className="nav-link" to="/register">Register</NavLink>
-              </button></>}
-
-            {isProfile && <><button type="button" className="btn btn-outline-warning ms-5 me-3"><NavLink className="nav-link" to="/profile" ><i className="fa fa-user" style={{ fontSize: "21px" }}></i></NavLink></button>
-              <button className="btn btn-warning" id="loginButton" type="button" onClick={logout}>Logout</button></>}
+    <>
+      <div className="App">
+        {users.map((User) => {
+          return <div>
+            <hr/>
+            <div><hi>Username: {User.username}</hi></div>
+            <div><hi>Email: {User.email}</hi></div>
+            <div><hi>Gender: {User.gender}</hi></div>
+            <div><hi>Pronouns: {User.pronouns}</hi></div>
+            <div><hi>Bio: {User.bio}</hi></div>
           </div>
-        </div>
-      </nav>
+        })}
+      </div>
+    </>
+    // <div className="App">
+    //   {/* Navbar */}
+    //   <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+    //     <div className="container-fluid">
 
-      {/* Webpage content */}
-      <header className="App-header">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
+    //       <img src={logo} alt="BEELINE logo" width="50" className="d-inline-block align-text-top app-icon" />
+    //       <NavLink className="navbar-brand text-warning app-name" to="/">BEELINE</NavLink>
 
-          <Route path="/login" element={<Login email={email} setEmail={setEmail} setUsername={setUsername} password={password} setPassword={setPassword} loginCredentials={accounts} />} />
+    //       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span className="navbar-toggler-icon"></span>
+    //       </button>
 
-          <Route path="/register" element={<Register email={email} setEmail={setEmail} username={username} setUsername={setUsername} password={password} setPassword={setPassword} registerCredentials={accounts} />} />
+    //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    //           <li className="nav-item">
+    //             <NavLink className="nav-link active" to="/">Dashboard</NavLink>
+    //           </li>
+    //         </ul>
 
-          <Route path="/profile" element={<Profile username={username} email={email} />} />
-        </Routes>
-      </header>
-    </div>
+    //         {isLogin && <><button className="btn btn-outline-warning ms-5 me-3" id="loginButton" type="button">
+    //           <NavLink className="nav-link" to="/login">Login</NavLink>
+    //         </button>
+    //           <button className="btn btn-warning" id="registerButton" type="button">
+    //             <NavLink className="nav-link" to="/register">Register</NavLink>
+    //           </button></>}
+
+    //         {isProfile && <><button type="button" className="btn btn-outline-warning ms-5 me-3"><NavLink className="nav-link" to="/profile" ><i className="fa fa-user" style={{ fontSize: "21px" }}></i></NavLink></button>
+    //           <button className="btn btn-warning" id="loginButton" type="button" onClick={logout}>Logout</button></>}
+    //       </div>
+    //     </div>
+    //   </nav>
+
+    //   {/* Webpage content */}
+    //   <header className="App-header">
+    //     <Routes>
+    //       <Route path="/" element={<Dashboard />} />
+
+    //       <Route path="/login" element={<Login email={email} setEmail={setEmail} setUsername={setUsername} password={password} setPassword={setPassword} loginCredentials={accounts} />} />
+
+    //       <Route path="/register" element={<Register email={email} setEmail={setEmail} username={username} setUsername={setUsername} password={password} setPassword={setPassword} registerCredentials={accounts} />} />
+
+    //       <Route path="/profile" element={<Profile username={username} email={email} />} />
+    //     </Routes>
+    //   </header>
+    // </div>
   );
 }
 
